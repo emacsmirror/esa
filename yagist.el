90c906542ccd5dabddb8743540a908ca9a5c2095
Masahiro Hayashi <mhayashi1120@gmail.com>
2013-01-14 23:46:35 +0900

    version up

--- yagist.el
+++ yagist.el
@@ -7,7 +7,7 @@
 ;; Michael Ivey
 ;; Phil Hagelberg
 ;; Dan McKinley
-;; Version: 0.8.1
+;; Version: 0.8.2
 ;; Created: 21 Jul 2008
 ;; Keywords: gist git github paste pastie pastebin
 ;; Package-Requires: ((json "1.2.0"))
