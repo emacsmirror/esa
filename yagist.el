9a7f69d3aa390d496dc0dd75fa0f5822abc20ca5
Masahiro Hayashi <mhayashi1120@gmail.com>
2013-08-23 19:20:06 +0900

    fix summary

--- yagist.el
+++ yagist.el
@@ -36,8 +36,8 @@
 ;;
 ;; http://bzr.savannah.gnu.org/lh/emacs/emacs-23/annotate/head:/lisp/json.el
 
-;; If you want to save encrypted token to ~/.gitconfig install elisp from
-;; following url.
+;; If you want to save encrypted token to ~/.gitconfig , install elisp from
+;; following url. Or Melpa.
 ;;
 ;; https://github.com/mhayashi1120/Emacs-kaesar/raw/master/kaesar.el
 ;;
