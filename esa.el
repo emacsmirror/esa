846ab3d970792cd741141f9e652a4c50aae3920c
Nab Inno <nab@blahfe.com>
2016-04-07 22:50:35 +0900

    Add how to configure esa.el to commentary.

--- esa.el
+++ esa.el
@@ -29,6 +29,15 @@
 
 ;;; Commentary:
 
+;; Get the Personal API Access Token from:
+;; https://yourteam.esa.io/user/tokens
+;;
+;; Put following to your .emacs:
+;;
+;; (require 'esa)
+;; (setq esa-token"******************************")
+;; (setq esa-team-name "yourteam")
+;;
 ;; TODO:
 ;; - Add esa-tokens-and-team-names defcustom
 ;; - Encrypt risky configs
